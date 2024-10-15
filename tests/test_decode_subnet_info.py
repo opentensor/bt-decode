94e07133638adb47ae3b13a861540101cca92645
Cameron Fairchild <cameron@opentensor.ai>
2024-10-15 14:08:50 -0400

    add tests for new option<subnetinfo> decoding

--- tests/test_decode_subnet_info.py
+++ tests/test_decode_subnet_info.py
@@ -14,6 +14,8 @@ from . import (
 
 TEST_SUBNET_INFO_HEX = {
     "normal": "0828feff010013ffffffffffffffff214e010104feff0300c8010401040d03a1050000c28ff4070398b6d54370c07a546ab0bab5ca9847eb5890ada1bda127633e607097ad4517dd2ca0f010",
+    "option normal": "010828feff010013ffffffffffffffff214e010104feff0300c8010401040d03a1050000c28ff4070398b6d54370c07a546ab0bab5ca9847eb5890ada1bda127633e607097ad4517dd2ca0f010",
+    "option none": "00",
     "vec option normal": lambda: get_file_bytes("tests/subnets_info.hex"),
 }
 
@@ -81,6 +83,22 @@ class TestDecodeSubnetInfo(unittest.TestCase):
                     )
 
         self.assertGreater(attr_count, 0, "No attributes found")
+
+    def test_decode_option_no_errors(self):
+        _ = bt_decode.SubnetInfo.decode_option(
+            bytes.fromhex(TEST_SUBNET_INFO_HEX["option normal"])
+        )
+
+    def test_decode_option_handles_none_some(self):
+        should_be_some = bt_decode.SubnetInfo.decode_option(
+            bytes.fromhex(TEST_SUBNET_INFO_HEX["option normal"])
+        )
+        self.assertIsNotNone(should_be_some)
+
+        should_be_none = bt_decode.SubnetInfo.decode_option(
+            bytes.fromhex(TEST_SUBNET_INFO_HEX["option none"])
+        )
+        self.assertIsNone(should_be_none)
 
     def test_decode_vec_option_no_errors(self):
         _ = bt_decode.SubnetInfo.decode_vec_option(
